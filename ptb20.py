19284f58cf903082d0d7bd13181aa27a2608bdf7
Proteseus <lewiwui@gmail.com>
2023-10-08 11:04:32 +0300

    text refactoring

--- ptb20.py
+++ ptb20.py
@@ -11,7 +11,7 @@ from db import create_user_order, add_order, delete_order, track, session
 from model import Order, Base
 
 from dotenv import load_dotenv
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton, Bot, InlineKeyboardButton, InlineKeyboardMarkup, BotCommand, MenuButtonCommands, MenuButton, BotCommandScopeChatMember
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton, Bot, InlineKeyboardButton, InlineKeyboardMarkup, BotCommand, MenuButtonCommands, MenuButton, BotCommandScopeChatMember, WebAppInfo
 from telegram.ext import Application, CallbackContext, CallbackQueryHandler, CommandHandler, ConversationHandler, filters, MessageHandler, Updater
 
 load_dotenv()
@@ -26,6 +26,14 @@ LOCALIZER, LOCATION, DETAILS, CONTACT, MORE_CONTACT, SUBSCRIPTION, SUBSCRIPTION_
 
 async def start(update: Update, context: CallbackContext) -> int:
     order = session.query(Order).filter(Order.username == update.effective_user.id).first()
+    
+    if str(update.effective_user.id) == os.getenv('USERNAME'):
+        await update.message.reply_text(
+            "ADMIN\n/generate_report\n/delete_subscriber",
+            reply_markup=ReplyKeyboardRemove()
+        )
+        return ConversationHandler.END
+    
     if order is None:
         Amharic = KeyboardButton(text="Amharic")
         English = KeyboardButton(text="English")
@@ -59,13 +67,83 @@ async def localizer(update: Update, context: CallbackContext) -> int:
     context.user_data['lang'] = lang
     logger.info("Language: %s", lang)
     
-    
-    await update.message.reply_text(
-    """Specify the service you want.\nIf its is a hotel or a guest house\nplease specify the name and the room or villa number""",
+    await context.bot.send_message(
+        text="""
+        *Pricing*
+
+*Item*                              *Price*
+
+*Tops*
+- All shirts.....................50
+- Polo.............................60
+- Blouse
+- Sweater......................100
+- Jacket/filled hight...250/350
+- Vest.............................50
+
+*Bottoms*
+- Trouser.........................80
+- Skirt L/s.....................250/200
+- Shorts..........................50
+- Underwear..................60
+
+*Full body*
+- National dress................350
+- Wedding dress L/S........2500/1800
+- Coat/jeans coat............300/200
+- Suit (jacket trouser).....400
+
+*Household*
+- Blanket Ex/L/m/s.........550/500/450/400
+- Duvet cover...................190
+- Comforter/duvet
+- Table cloth s/m/l..........75/100/125
+- Towel s/m/l....................30/40/60
+
+*Accessories*
+- Napkin............................30
+- Pillow case.....................30
+- Tie/scarf.........................30
+
+*Button repair*
+- Free
+
+        """,
+        chat_id=update.effective_chat.id,
+        parse_mode="Markdown",
         reply_markup=ReplyKeyboardRemove()
     )
+    
+    if lang == "English":
+        await update.message.reply_text(
+        """Name\nSecondary phone\nName of your area""",
+            reply_markup=ReplyKeyboardRemove()
+        )
+    else:
+        await update.message.reply_text(
+        """ሙሉ ስም\nያሉበት ሰፈር ስም""",
+            reply_markup=ReplyKeyboardRemove()
+        )
+        
     return DETAILS
 
+async def details(update: Update, context: CallbackContext) -> int:
+    """Store details and ask for contact"""
+    user_details = update.message.text
+    logger.info("Details: %s", user_details)
+    
+    context.user_data['details'] = user_details
+    
+    location_keyboard = KeyboardButton(text="Share Location", request_location=True)
+    custom_keyboard = [[ location_keyboard ]]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
+    
+    await update.message.reply_text(
+        'Hi! Please share your location by pressing the button below.',
+        reply_markup=reply_markup
+    )
+    
+    return LOCATION
 
 async def location(update: Update, context: CallbackContext) -> int:
     """Store user location and ask for contact"""
@@ -87,24 +165,6 @@ async def location(update: Update, context: CallbackContext) -> int:
     )
 
     return CONTACT
-
-async def details(update: Update, context: CallbackContext) -> int:
-    """Store details and ask for contact"""
-    user_details = update.message.text
-    logger.info("Details: %s", user_details)
-    
-    context.user_data['details'] = user_details
-    
-    location_keyboard = KeyboardButton(text="Share Location", request_location=True)
-    custom_keyboard = [[ location_keyboard ]]
-    reply_markup = ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
-    
-    await update.message.reply_text(
-        'Hi! Please share your location by pressing the button below.',
-        reply_markup=reply_markup
-    )
-    
-    return LOCATION
 
 async def contact(update: Update, context: CallbackContext) -> int:
     """Store user contact and ask for additional"""
@@ -145,9 +205,29 @@ async def more_contact(update: Update, context: CallbackContext) -> int:
     no = KeyboardButton(text="No")
     custom_keyboard = [[ subscribe, no ]]
     reply_markup = ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
-    await update.message.reply_text(
-        'Hi! Would you like to subscribe to our services:',
-        reply_markup=reply_markup
+    await context.bot.send_message(
+        text="""Hi! Would you like to subscribe to our services:
+        
+*Benefits*:
+*Ocean subscriber*
+
+- Our simplest plan priced by the kilogram and always free pickup and delivery. Enjoy savings of up to 40% vs. order once in a while.
+- Priced by the bag, as low as 1500/bag.
+- If it fits 25 kg, we’ll clean it.
+- Always free pickup and delivery.
+- Next day rush service available for double price.
+
+*Once in a while service*
+
+If you only need our wash fold services every once in a while, this is the choice for you. It’s a great same service.
+
+- Priced by the pieces.
+- Free pickup and delivery with a minimum of 2000 birr.
+- Next day rush service available for 250 birr delivery payment.
+        """,
+        chat_id=update.effective_chat.id,
+        reply_markup=reply_markup,
+        parse_mode="Markdown"
     )
     
     return SUBSCRIPTION
@@ -186,18 +266,16 @@ async def subscription_type(update: Update, context: CallbackContext) -> int:
     
     return await order_detail(update, context)
 
-async def cancel(update: Update, context: CallbackContext) -> int:
-    """Cancels and ends the conversation."""
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    await update.message.reply_text(
-        "Bye! I hope we can talk again some day.",
-        reply_markup=ReplyKeyboardRemove()
-    )
-    return ConversationHandler.END
-
 async def cancel_sub(update: Update, context: CallbackContext):
     """Cancels and ends the conversation."""
+    
+    if str(update.effective_user.id) == os.getenv('USERNAME'):
+        await update.message.reply_text(
+            "ADMIN\n/generate_report\n/delete_subscriber",
+            reply_markup=ReplyKeyboardRemove()
+        )
+        return ConversationHandler.END
+    
     user = update.message.from_user
     logger.info("User %s wants to cancel their subscription.", f"{user.first_name}: {user.id}")
     
@@ -216,11 +294,6 @@ async def cancel_sub(update: Update, context: CallbackContext):
         await update.message.reply_text(
             "Subscription not found.\nYou can't cancel a subscription that you don't have.\nIf you want to subscribe, please reply with /start.",
         )
-
-async def error_handler(update: Update, context: CallbackContext):
-    """Log the error and handle it gracefully"""
-    logger.error(msg="Exception occurred", exc_info=context.error)
-    await update.message.reply_text('Sorry, an error occurred. Please try again.')
 
 async def order_detail(update: Update, context: CallbackContext) -> int:
     """Store details and pass them to admins"""
@@ -259,7 +332,32 @@ async def send_details(update: Update, context: CallbackContext, sub: False, ord
     tracker_id = track(order_details['user_id'])
 
     await update.message.reply_text(
-        f"Your order #{tracker_id} has been submitted.\nWe'll be in touch!",
+        text=f"""
+        Order `#{tracker_id}` accepted
+
+The courier will write you in advance about the time of the arrival.
+
+Payment for the order occurs at the arrival of the washed items.
+
+Orders will be picked on the day of the order
+
+Repair service for missing or broken buttons is complementary with any ocean dry cleaning order.
+
+We pick up and deliver 7 days a week, always between 6AM and 10PM.
+
+*Pickup and delivery fees*
+The 190 birr delivery fee will only be paid if your order is below 1000 birr.
+
+*Free delivery*
+Get unlimited free pickups and delivery by subscribing our packages
+
+Become a subscriber now on *Ocean*.
+
+Save time and your sanity when you leave the dry cleaning errands to us.
+
+Call `4840` for any help
+        """,
+        parse_mode='markdown',
         reply_markup=ReplyKeyboardRemove())
     
     username = os.getenv('USERNAME')
@@ -330,11 +428,90 @@ async def get_chat_id(update, context):
     user_id = update.message.from_user.id
     await update.message.reply_text(f"Your chat ID is {chat_id} - user ID is {user_id}")
 
+async def about(update: Update, context: CallbackContext) -> int:
+    """Sends a message with "about" info"""
+    await context.bot.send_message(
+    text="""*How it works*
+
+1. *Order Placement*
+   - We come and pick up your clothes when you make a call to our call center or directly make your order in our telegram bot. We then deliver it at the right time.
+
+2. *Photo and Inventory*
+   - We text you a photo and itemized inventory of what we pick up, so you remember what’s being cleaned and in what condition.
+
+3. *Spot and Stain Inspection*
+   - We carefully inspect for spots and stains. Our spotters have decades of experience in identifying and treating stains in the best way possible so your garments are returned pristine.
+
+4. *Expert Cleaning*
+   - We clean your clothes with expert care. We follow the care label (and know what all the symbols mean) so your clothes receive the optimal cleaning treatment and last for years to come.
+
+5. *Pressing and Hanging*
+   - We press and hang each of your items. Your clothes are crispy pressed, put on hangers, and placed in your protective garment bag, ready to wear when we deliver your dry cleaning to your door.
+
+*Wash and Fold*
+
+- Wash & fold is the perfect service to use if you want to avoid doing laundry and save your time and your sanity.
+- Ocean will pick up, clean, and deliver your laundry right back to your door. Your clothes get their own machine, are cleaned according to your preference, and delivered neatly folded - we even pair your socks.
+- Let ocean do your laundry for you so you can focus on more important things.
+
+    *How it works*
+
+    1. *Inspecting Clothes and Checking Pockets*
+    - We inspect your clothes and check your pockets. We do pocket inspection for you so nothing ends up in the wash that shouldn’t. All pockets and clothes are inspected before being washed.
+
+    2. *Careful Washing*
+    - We clean your items with extra care. Your light and darks are separated and all your clothes are washed using cold water to preserve color (and save energy).
+
+    3. *Customized Laundry*
+    - We wash your loads according to your choices. Want a fabric softener? Just select the laundry preferences that are right for you.
+
+    4. *Folding Service*
+    - We fold everything so that you don’t have to. Your clothes are crisply folded, and your socks are paired, ready to be worn or put away when we deliver your clothes to your door.
+
+""",
+chat_id=update.effective_chat.id,
+parse_mode='Markdown'
+    )
+
+async def subscription(update: Update, context: CallbackContext) -> int:
+    """Sends a message with "about" info"""
+    await context.bot.send_message(
+    text="""
+    # Subscription
+
+    """,
+    parse_mode='Markdown',
+    chat_id=update.effective_chat.id
+    )
+
+async def cancel(update: Update, context: CallbackContext) -> int:
+    """Cancels and ends the conversation."""
+    
+    if str(update.effective_user.id) == os.getenv('USERNAME'):
+        await update.message.reply_text(
+            "ADMIN\n/generate_report\n/delete_subscriber",
+            reply_markup=ReplyKeyboardRemove()
+        )
+        return ConversationHandler.END
+    
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.",
+        reply_markup=ReplyKeyboardRemove()
+    )
+    return ConversationHandler.END
+
+async def error_handler(update: Update, context: CallbackContext):
+    """Log the error and handle it gracefully"""
+    logger.error(msg="Exception occurred", exc_info=context.error)
+    await update.message.reply_text('Sorry, an error occurred. Please try again.')
+
 ########################################################
 def main():
     # bot runner
     application = Application.builder().token(TOKEN).build()
-
+    
     # Commands
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
@@ -355,6 +532,8 @@ def main():
     application.add_handler(CommandHandler("get_chat_id", get_chat_id))
     application.add_handler(CommandHandler("delete_subscriber", delete_subscriber))
     application.add_handler(CommandHandler("generate_report", generate_report))
+    # application.add_handler(CommandHandler("subscription", subscription))
+    application.add_handler(CommandHandler("about", about))
 
     # Run bot
     application.run_polling(allowed_updates=Update.ALL_TYPES, poll_interval=1.0)
