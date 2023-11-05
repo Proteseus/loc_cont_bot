8ffcec924563fa16e11f2c7fab743bbdcd8b19df
Proteseus <lewiwui@gmail.com>
2023-11-05 10:30:13 +0300

    Modified report times

--- scheduler.py
+++ scheduler.py
@@ -17,11 +17,14 @@ def check_subscribers():
     print("subs")
     subprocess.run(['python3', 'subscription_notice.py'])
 
-# Schedule the task to run at midnight
-schedule.every().day.at("21:00").do(run_subs_reports)
+# Schedule the task to run at 10PM
+schedule.every().day.at("19:00").do(run_subs_reports)
 
-# Schedule the task to run at midnight
-schedule.every().day.at("19:00").do(run_ord_reports)
+# Schedule the task to run at 6AM
+schedule.every().day.at("03:00").do(run_ord_reports)
+
+# Schedule the task to run at 12PM
+schedule.every().day.at("09:00").do(run_ord_reports)
 
 # Schedule the task to run at 6AM
 schedule.every().day.at("03:00").do(check_subscribers)
